a860ac2260b04a604e0cb21a4df055716d697459
Huilde Viana Junior <hvianajunior@apple.com>
2023-05-18 17:32:12 -0300

    First commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Card = (props) => {
+  const { title, button, text, svg, projeto } = props;
+  const classe = `card ${projeto}`;
+  return (
+    <div className={classe}>
+      <div className="title">
+        {svg} {title}
+      </div>
+      <div className="principal-text">{text}</div>
+      <button className="card-button"> {button}</button>
+    </div>
+  );
+};
+
+export { Card };
