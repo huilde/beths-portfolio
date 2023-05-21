6296283d8a9332b58206349df38cb768c9c338bb
Huilde Viana Junior <hvianajunior@apple.com>
2023-05-20 22:17:12 -0300

    fix: adjust grid sistem and add links do buttons

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,18 @@
+import { Col, Row } from "antd";
 import React from "react";
 
 const Card = (props) => {
-  const { title, button, text, svg, projeto } = props;
+  const { title, button, text, svg, projeto, link } = props;
   const classe = `card ${projeto}`;
   return (
     <div className={classe}>
       <div className="title">
         {svg} {title}
       </div>
+      <Col flex="auto"></Col>
       <div className="principal-text">{text}</div>
-      <button className="card-button">
-        <p>{button}</p>
+      <button onClick={() => link()} className="card-button">
+        {button}
       </button>
     </div>
   );
