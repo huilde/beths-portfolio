a9cde2da2ec8dd4e26fbd6c6a3cb04cafd4e1f00
Huilde Viana Junior <hvianajunior@apple.com>
2023-05-22 21:01:21 -0300

    fix: adjust vite-config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default {
-  // Other Vite configuration options...
+export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
       external: ["styled-components"],
     },
   },
-};
+});
